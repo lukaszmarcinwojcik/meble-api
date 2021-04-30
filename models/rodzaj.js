3f229be9bbd548d4dfd539fad35fb0fa80d6e9bc
lukaszmarcinwojcik <lukaszmarcinwojcik@gmail.com>
2021-04-30 15:07:10 +0200

    add delete

--- models/rodzaj.js
+++ models/rodzaj.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 const rodzajSchema = new Schema({
   nazwa: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Rodzaj", rodzajSchema);
