f27e4e608642662744605d46abbeb5865229ed1e
lukaszmarcinwojcik <lukaszmarcinwojcik@gmail.com>
2021-07-01 13:09:07 +0200

    cors

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.set("view engine", "jade");
 
 // app.use(logger("dev"));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   cors({
     origin: ["https://meble-klasyczne.herokuapp.com/"],
